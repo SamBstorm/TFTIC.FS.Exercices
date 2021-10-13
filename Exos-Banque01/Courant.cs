a061b9aa449fe3a02c52a47a2212b31d6562c116
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-13 16:52:16 +0200

    Ajoutez des fichiers projet.

---
+++ Exos-Banque01/Courant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exos_Banque01
+{
+    class Courant
+    {
+        public string Numero { get; set; }
+        public double Solde { get; private set; }
+
+        #region Si le solde n'était pas une auto-propriété
+        //private double _solde;
+
+        //public double Solde
+        //{
+        //    get { return _solde; }
+        //} 
+        //ATTENTION : SI la propriété Solde n'a pas de SET, alors les lignes 39 et 44 doivent affecter les valeurs au champs _solde
+        #endregion
+
+        public Personne Titulaire { get; set; }
+
+        private double _ligneDeCredit;
+
+        public double LigneDeCredit
+        {
+            get { return _ligneDeCredit; }
+            set { 
+                if(value >= 0) _ligneDeCredit = value;
+                //signaler erreur
+            }
+        }
+
+        public void Retrait(double montant) {
+            if (montant <= 0) return;   //signaler erreur
+            if( Solde + LigneDeCredit >= montant) Solde = Solde - montant;
+            //signaler l'erreur
+        }
+        public void Depot(double montant) {
+            if (montant <= 0) return;   //signaler erreur
+            Solde = Solde + montant;
+        }
+
+    }
+}
---
+++ Exos-Banque01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exos_Banque01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne p1 = new Personne();
+            p1.Prenom = "Samuel";
+            p1.Nom = "Legrain";
+            p1.DateNaiss = new DateTime(1987, 9, 27);
+
+            Courant c1 = new Courant();
+            c1.Numero = "BE97 0000 0000 0000";
+            c1.LigneDeCredit = 200;
+            c1.Titulaire = p1;
+
+            Console.WriteLine(c1);
+
+            Console.WriteLine($"Compte N° {c1.Numero}\nTitulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom} né en {c1.Titulaire.DateNaiss.Year}");
+            Console.WriteLine($"Solde actuel : {c1.Solde}\tLigne de crédit : {c1.LigneDeCredit}");
+
+            c1.Depot(500);
+
+            Console.WriteLine($"Compte N° {c1.Numero}\nTitulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom} né en {c1.Titulaire.DateNaiss.Year}");
+            Console.WriteLine($"Solde actuel : {c1.Solde}\tLigne de crédit : {c1.LigneDeCredit}");
+
+            c1.Retrait(1000);
+
+            Console.WriteLine($"Compte N° {c1.Numero}\nTitulaire : {c1.Titulaire.Nom} {c1.Titulaire.Prenom} né en {c1.Titulaire.DateNaiss.Year}");
+            Console.WriteLine($"Solde actuel : {c1.Solde}\tLigne de crédit : {c1.LigneDeCredit}");
+
+        }
+    }
+}
