c2dd321790226caed19ce07d9d00533a4fd5b45f
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-14 11:50:16 +0200

    Add Exos02

---
+++ Exos-Banque02/Banque.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exos_Banque01
+{
+    class Banque
+    {
+        public string Nom { get; set; }
+
+        private Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+
+        public Courant this[string numero]
+        {
+            get
+            {
+                Courant compte;
+                _comptes.TryGetValue(numero, out compte);
+                return compte;
+            }
+
+            set {
+                if(value != null && value.Numero == numero)
+                _comptes[numero] = value;
+            }
+        }
+
+        public void Ajouter(Courant compte) {
+            if(compte != null)
+            _comptes.Add(compte.Numero, compte);
+        }
+
+        public void Supprimer(string numero) {
+            if(numero != null)
+            _comptes.Remove(numero);
+        }
+
+    }
+}
---
+++ Exos-Banque02/Personne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exos_Banque01
+{
+    class Personne
+    {
+        private string _nom;
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public string Prenom { get; set; }
+
+        public DateTime DateNaiss { get; set; }
+    }
+}
